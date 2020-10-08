916a295e8fb05203bc50c675bad80f2bba0084e5
BurhanMuhyiddin <burhanbhos1997@gmail.com>
2020-10-08 15:42:59 +0200

    applied PID to angular and linear velocity

--- dd_robot_motion_tracking/scripts/detect_motion.py
+++ dd_robot_motion_tracking/scripts/detect_motion.py
@@ -13,16 +13,22 @@ class MotionDetector(object):
         self.pub = rospy.Publisher('offset', Float64, queue_size=10)
         rospy.init_node('motion_tracker_node')
         self.rate = rospy.Rate(10)
+
+        self.offset = 0.0
+
         res, self.handle_cam = sim.simxGetObjectHandle(self.clientID, "cam", sim.simx_opmode_blocking)
+        
         self.track_motion()
 
     def track_motion(self):
         res, resolution, frame1 = sim.simxGetVisionSensorImage(self.clientID, self.handle_cam, 0, sim.simx_opmode_streaming)
         
+        # wait until frame is read correctly
         while res != sim.simx_return_ok: 
             res, resolution, frame1 = sim.simxGetVisionSensorImage(self.clientID, self.handle_cam, 0, sim.simx_opmode_buffer)
         res, resolution, frame2 = sim.simxGetVisionSensorImage(self.clientID, self.handle_cam, 0, sim.simx_opmode_buffer)
 
+        # convert from BGR to RGB, also the position of the image
         frame1 = frame1[::-1]
         frame1 = np.array(frame1, dtype=np.uint8)
         frame1.resize([resolution[0], resolution[1], 3])
@@ -31,6 +37,7 @@ class MotionDetector(object):
         frame2 = np.array(frame2, dtype=np.uint8)
         frame2.resize([resolution[0], resolution[1], 3])
 
+        # Process the video and detect motion by subtracting subsequent frames
         while not rospy.is_shutdown():
             if sim.simxGetConnectionId(self.clientID) != -1:
                 diff = cv2.absdiff(frame1, frame2)
@@ -44,24 +51,24 @@ class MotionDetector(object):
                 centerX = 0
 
                 for contour in contours:
-                    #print(cv2.contourArea(contour))
                     (x, y, w, h) = cv2.boundingRect(contour)
                     if w >= h or cv2.contourArea(contour) < 1000: # because human's height is greater than width
                         continue
                     counter = counter + 1
                     centerX = centerX + (2.0 * x + w) / 2.0
-                    cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 1)
+                    #cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 1)
                 
                 if counter != 0:
                     centerX = centerX / counter
+                    self.offset = (resolution[0] / 2.0) - centerX
+                else:
+                    self.offset = 0.0
 
-                offset = (resolution[0] / 2.0) - centerX
-                self.pub.publish(offset)
+                self.pub.publish(self.offset) # publish the offset from the center
 
-                #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 1)
-
-                cv2.imshow("img", frame1)
-
+                cv2.imshow("camera", frame1)
+                
+                # update variables
                 frame1 = frame2
                 res, resolution, frame2 = sim.simxGetVisionSensorImage(self.clientID, self.handle_cam, 0, sim.simx_opmode_buffer)
                 
--- dd_robot_motion_tracking/scripts/drive_DDrobot.py
+++ dd_robot_motion_tracking/scripts/drive_DDrobot.py
@@ -4,61 +4,69 @@ import sim
 import rospy
 from std_msgs.msg import Float64
 import math
+import time
 
 class Drive_Robot(object):
     def __init__(self, clientID):
+        # declare some robot parameters
         self.clientID = clientID
         self.WHEEL_RADIUS = 0.0975
         self.DISTANCE_BETWEEN_WHEELS = 0.3810
-        self.ROBOT_VELOCITY = 0.10
-        self.STEER_ANGLE = math.pi / 30.0
-        self.Kp_a = 0.0015
-        self.Kp_v = 0.04
-        self.velocity = 0.2
+        self.dist_from_Bill = 4.0
+        
+        # declare PID gains for the angle control
+        self.Kp_a = 0.0015  # Proportional gain
+        self.Kd_a = 0.00002 # Derivative gain
+        self.Ki_a = 0.00035 # Integral gain
+        self.integral_term = 0.0
+
+        # declare P gain for the velocity control
+        self.Kp_v = 0.2
+
+        self.velocity = 0.0
+        self.steer_angle = 0.0
+        
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        self.last_offset = 0.0
         
         res, self.handle_right_wheel = sim.simxGetObjectHandle(self.clientID, "Pioneer_p3dx_rightMotor", sim.simx_opmode_blocking)
         res, self.handle_left_wheel = sim.simxGetObjectHandle(self.clientID, "Pioneer_p3dx_leftMotor", sim.simx_opmode_blocking)
         res, self.handle_DDrobot = sim.simxGetObjectHandle(self.clientID, "Pioneer_p3dx", sim.simx_opmode_blocking)
-        res, self.handle_person = sim.simxGetObjectHandle(self.clientID, "Bill", sim.simx_opmode_blocking)
+        res, self.handle_distance = sim.simxGetDistanceHandle(self.clientID, "Distance", sim.simx_opmode_blocking)
+        
+        res, self.distance = sim.simxReadDistance(self.clientID, self.handle_distance, sim.simx_opmode_streaming)
 
         rospy.Subscriber('/offset', Float64, self.set_joint_angles)
-        res, self.dist_DDrobot = sim.simxGetObjectPosition(self.clientID, self.handle_DDrobot, self.handle_person, sim.simx_opmode_streaming)
 
     def set_joint_angles(self, data):
-        steer_angle = 0.0
         offset = data.data
-        res, dist_DDrobot = sim.simxGetObjectPosition(self.clientID, self.handle_DDrobot, self.handle_person, sim.simx_opmode_buffer)
+        res, self.distance = sim.simxReadDistance(self.clientID, self.handle_distance, sim.simx_opmode_buffer) # read distance between
+                                                                                                               # robot and Bill 
         
-        dist = math.sqrt(dist_DDrobot[0] * dist_DDrobot[0] + dist_DDrobot[1] * dist_DDrobot[1]) 
-        if dist <= 3.0:
-            #self.velocity = 0.0
-            self.velocity = self.Kp_v * dist * -1.0
-        elif dist >= 3.5:
-            self.velocity = self.Kp_v * dist
+        self.current_time = time.time()
+        delta_time = self.current_time - self.last_time
+        delta_offset = offset - self.last_offset
 
-        #if abs(offset) <= 0.1:
-        #    steer_angle = 0.0
-        #elif offset == 999999999.0:
-        #    self.velocity = 0.0
-        #    steer_angle = math.pi/90
-        #elif abs(offset) >= 0.5:
-        #    k = abs(offset) / (offset) * (-1.0) # get the sign of the offset to control the robot (+: turn right, -: turn left)
-                                                # multiplying by -1 is just for accomodating for the simulation
-        #    steer_angle = k * self.Kp_a * self.STEER_ANGLE
+        self.velocity = self.Kp_v * (self.dist_from_Bill - self.distance) * (-1.0) # try to keep robot a bit away from Bill, so can process him in camera
 
-        steer_angle = self.Kp_a * offset * (-1.0)
-
-        rightWheelVelocity = self.velocity + (self.DISTANCE_BETWEEN_WHEELS) * steer_angle
-        leftWheelVelocity = self.velocity - (self.DISTANCE_BETWEEN_WHEELS) * steer_angle
+        # Calculate the PID terms and control the angle properly, so robot can try to keep Bill in the center of camera
+        self.integral_term = self.integral_term + offset * delta_time
+        self.steer_angle = ( self.Kp_a * offset + self.Kd_a * (delta_offset / delta_time) + self.Ki_a * self.integral_term) * (-1.0)
+        
+        # Calculate the kinematics for left and right wheels
+        rightWheelVelocity = self.velocity + (self.DISTANCE_BETWEEN_WHEELS) * self.steer_angle
+        leftWheelVelocity = self.velocity - (self.DISTANCE_BETWEEN_WHEELS) * self.steer_angle
         rightAngularVelocity = rightWheelVelocity / self.WHEEL_RADIUS
         leftAngularVelocity = leftWheelVelocity / self.WHEEL_RADIUS
         
-        #rospy.loginfo(rightAngularVelocity)
-        #rospy.loginfo(leftAngularVelocity)
-
+        # Send the joint values to the joints
         sim.simxSetJointTargetVelocity(self.clientID, self.handle_right_wheel, rightAngularVelocity, sim.simx_opmode_streaming)
         sim.simxSetJointTargetVelocity(self.clientID, self.handle_left_wheel, leftAngularVelocity, sim.simx_opmode_streaming)
-
+        
+        # update variables
+        self.last_time = self.current_time
+        self.last_offset = offset
 
 def main():
     rospy.init_node('drive_node')
